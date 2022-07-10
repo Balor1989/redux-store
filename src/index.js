0f006c6e7fb0a01c6d6c141599260ce4ff9bc663
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-07-10 22:20:58 +0300

    fix changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+    case "DEC":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({ type: "INC" });
+
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC" });
+
+store.dispatch({ type: "DEC" });
