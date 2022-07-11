4e0ea7bedeb3a076300614085810f079e4cca3e2
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-07-11 11:52:14 +0300

    fix changes

---
+++ src/counter.js
@@ -0,0 +1,12 @@
+const Counter = () => {
+  return (
+    <div className="container">
+      <h2 className="counter">0</h2>
+      <button className="dec">-</button>
+      <button className="inc">+</button>
+      <button className="random">random</button>
+    </div>
+  );
+};
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
-import { createStore } from "redux";
+import { createStore, bindActionCreators } from "redux";
 import { reducer } from "./reducer";
-import { inc, dec, random } from "./actions";
+import * as actions from "./actions";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Counter from "./counter";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Counter />
+  </React.StrictMode>
+);
 
 const store = createStore(reducer);
+const { dispatch } = store;
 
-document.querySelector(".inc").addEventListener("click", () => {
-  store.dispatch(inc());
-});
+// const bindActionCreator =
+//   (creator, dispatch) =>
+//   (...args) => {
+//     dispatch(creator(...args));
+//   };
 
-document.querySelector(".dec").addEventListener("click", () => {
-  store.dispatch(dec());
-});
+// const incDispatch = bindActionCreators(inc, dispatch);
+// const decDispatch = bindActionCreators(dec, dispatch);
+// const randomDispatch = bindActionCreators(random, dispatch);
+
+const { inc, dec, random } = bindActionCreators(actions, dispatch);
+
+document.querySelector(".inc").addEventListener("click", inc);
+
+document.querySelector(".dec").addEventListener("click", dec);
+
 document.querySelector(".random").addEventListener("click", () => {
   const payload = Math.floor(Math.random() * 10);
-  store.dispatch(random(payload));
+  random(payload);
 });
 
 const update = () => {
