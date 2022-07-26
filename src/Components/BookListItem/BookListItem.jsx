3be64b2d5adbe2c3e580c77623410879230f87d0
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-07-26 21:39:56 +0300

    adds BookList and BookListItem components

---
+++ src/Components/BookListItem/BookListItem.jsx
@@ -0,0 +1,5 @@
+const BookListItem = ({ book }) => {
+  return <span>{book.title}</span>;
+};
+
+export default BookListItem;
--- src/Views/HomePage/HomePage.jsx
+++ src/Views/HomePage/HomePage.jsx
@@ -1,5 +1,18 @@
+import BookList from "../../Components/BookList";
+
 const HomePage = () => {
-  return <div>Home page</div>;
+  const books = [
+    {
+      id: 1,
+      title: "first book",
+    },
+    {
+      id: 2,
+      title: "second book",
+    },
+  ];
+
+  return <BookList books={books} />;
 };
 
 export default HomePage;
