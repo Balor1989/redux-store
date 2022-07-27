de71d370475a7c67e9bfaaef68b4c0b148a7c0d2
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-07-27 18:44:58 +0300

    adds header styles

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,18 @@
 import s from "./Header.module.css";
 import { MdShoppingCart } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { IconContext } from "react-icons/lib";
 
 const Header = ({ items, total }) => {
   return (
-    <header>
-      <Link to="/">$BookStore</Link>
-      <Link to="/cart">
-        <MdShoppingCart />
+    <header className={`${s.headerContainer} container`}>
+      <Link to="/" className={s.logo}>
+        $BookStore
+      </Link>
+      <Link to="/cart" className={s.cart}>
+        <IconContext.Provider value={{ className: `${s.icon}` }}>
+          <MdShoppingCart />
+        </IconContext.Provider>
         {items} items (${total})
       </Link>
     </header>
